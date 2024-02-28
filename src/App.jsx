46e04f5acdceb85b8f5906ada0948cce5119170b
Cheeseman757 <156013482+Cheeseman757@users.noreply.github.com>
2024-02-28 13:46:28 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import 'App.css'
-import Header from 'header.jsx'
-import Gallery from 'gallery.jsx'
-import Footer from 'Footer.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Header from './Header.jsx';
+import Gallery from './Gallery.jsx';
+import Footer from './Footer.jsx';
+
 
 function App() {
 
   return (
-    <>
+    <div>
       <Header />
       <Gallery />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
