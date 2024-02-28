c2fabbc147358e8b8236a994215d4855bebdfe9d
Cheeseman757 <156013482+Cheeseman757@users.noreply.github.com>
2024-02-28 14:13:10 -0800

    Update hornedbeasts.jsx

--- src/hornedbeasts.jsx
+++ src/hornedbeasts.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import hornedBeasts from "./hornedBeasts";
+import hornedbeasts from "./hornedbeasts";
 import data from './assets/data.json';
 import Row from 'react-bootstrap/Row';
 
