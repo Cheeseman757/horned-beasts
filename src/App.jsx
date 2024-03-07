1bf4ca540e51033b3af1364748552c3315506b47
Cheeseman757 <156013482+Cheeseman757@users.noreply.github.com>
2024-03-07 08:45:14 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './header.jsx';
-import Gallery from './gallery.jsx';
-import Footer from './Footer.jsx';
 
+import './App.css'
+import Header from './header.jsx'
+import Gallery from './gallery.jsx'
+import Footer from './footer.jsx'
+import SelectedBeast from './SelectedBeast.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import beastData from "./assets/data.json"
+import { useState } from 'react'
 
 function App() {
+  let [show, setShow] = useState(false);
+  let [beast, setBeast] = useState(null);
+  
+  const showModal = obj => {
+    setBeast(obj)
+    setShow(true); 
+  }
+
+  const closeModal = () => {
+    setShow(false);
+  }
 
   return (
-    <div>
+    <>
       <Header />
-      <Gallery />
+      <SelectedBeast showBool={show} onHide={closeModal} selectBeast={beast} />
+      <Gallery beastData={beastData} onClick={showModal}  />
       <Footer />
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
