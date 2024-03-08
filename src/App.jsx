7c0b409e52499209c0688a3fc1db0253adf5f33d
Justin Cheeseman <156013482+Cheeseman757@users.noreply.github.com>
2024-03-07 22:15:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './header.jsx'
 import Gallery from './gallery.jsx'
 import Footer from './Footer.jsx'
-import SelectedBeast from './selectedbeasts.jsx'
+import SelectedBeast from './Selectedbeasts.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import beastData from "./assets/data.json"
@@ -25,7 +25,7 @@ function App() {
   return (
     <>
       <Header />
-      <selectedbeasts showBool={show} onHide={closeModal} selectBeast={beast} />
+      <selectedbeasts showbool={show} onHide={closeModal} selectbeast={beast} />
       <Gallery beastData={beastData} onClick={showModal}  />
       <Footer />
     </>
